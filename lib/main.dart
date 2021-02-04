d3ac3518765e8ca3398c84ec0719fccb65b64b1d
mrCasus <osmanaltuntas@hotmail.com>
2021-02-04 13:37:57 +0300

    firebase
    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutterchartss/screens/homeScreen.dart';
-import 'package:custom_splash/custom_splash.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+//import 'package:flutterchartss/screens/homeScreen.dart';
+
+import 'package:splash_screen_view/SplashScreenView.dart';
+import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,23 +18,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MobimAsistan',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        debugShowCheckedModeBanner: false,
+        home: SplashScreenView(
+          home: MyHomePage(),
+          duration: 5000,
+          imageSize: 500,
+          imageSrc: "assets/caps-product-pmsa.png",
+          backgroundColor: Colors.white,
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          'deneme',
+          style: TextStyle(
+              fontFamily: 'Montserrat', fontSize: 25.0, color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+          // iconSize: 17.0,
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(FontAwesomeIcons.doorOpen),
+            color: Colors.black,
+            onPressed: () {},
+          )
+        ],
       ),
-      debugShowCheckedModeBanner: false,
-      home: CustomSplash(
-        imagePath: 'assets/caps-product-pmsa.png',
-        //backGroundColor: Colors.deepOrange,
-        //backGroundColor: Color(0xfffc6042),
-        animationEffect: 'fade-in',
-        logoSize: 700,
-        home: HomeScreen(),
-        // customFunction: duringSplash,
-        duration: 2500,
-        type: CustomSplashType.StaticDuration,
-        // outputAndHome: op,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Demonstration',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: FFNavigationBar(
+        theme: FFNavigationBarTheme(
+          barBackgroundColor: Colors.white,
+          // selectedItemBorderColor: Colors.black,
+          selectedItemBackgroundColor: Colors.white,
+          selectedItemIconColor: Colors.black,
+          selectedItemLabelColor: Colors.black,
+          showSelectedItemShadow: false,
+          barHeight: 70,
+        ),
+        selectedIndex: selectedIndex,
+        onSelectTab: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        items: [
+          FFNavigationBarItem(
+            iconData: FontAwesomeIcons.home,
+            label: 'Home',
+          ),
+          FFNavigationBarItem(
+            iconData: FontAwesomeIcons.clipboardList,
+            label: 'Demirbaş Listesi',
+            selectedBackgroundColor: Colors.orange,
+          ),
+          FFNavigationBarItem(
+            iconData: FontAwesomeIcons.tools,
+            label: 'Bakım Kartı',
+            selectedBackgroundColor: Colors.purple,
+          ),
+        ],
       ),
     );
   }
